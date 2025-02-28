aed2740f259c3de1252177215d77cad7a30a430f
JCamiloCardonaZ <juancamilo.cardona1@utp.edu.co>
2025-02-27 21:53:05 -0500

    Hechos

---
+++ grafo.pl
@@ -0,0 +1,10 @@
+% Definir las conexiones del grafo con su costo
+conexion(vancouver, edmonton, 16).
+conexion(vancouver, calgary, 13).
+conexion(edmonton, saskatoon, 12).
+conexion(saskatoon, winnipeg, 20).
+conexion(saskatoon, regina, 7).
+conexion(calgary, regina, 14).
+conexion(regina, winnipeg, 4).
+conexion(regina, saskatoon, 7).
+
