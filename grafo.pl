c712b89c9bec24b7d67cdb1333ab6a1edc41139e
Juan Camilo Gallego <camilo.gallego3@utp.edu.co>
2025-02-27 21:56:38 -0500

    REGLAS DE CONEXIONES

--- grafo.pl
+++ grafo.pl
@@ -8,3 +8,13 @@ conexion(calgary, regina, 14).
 conexion(regina, winnipeg, 4).
 conexion(regina, saskatoon, 7).
 
+% Regla para verificar si hay una conexión directa entre dos nodos
+conexion_directa(A, B) :- conexion(A, B, _).
+
+% Regla para verificar si hay una conexión (directa o indirecta) entre dos nodos
+conexion_indirecta(A, B) :- conexion(A, B, _).
+conexion_indirecta(A, B) :- conexion(A, C, _), conexion_indirecta(C, B).
+
+% Regla para saber las conexiones de un nodo con sus respectivos costos
+% se usa el ya conocido functor y a la variable anónima se le pasa costo como nombre: 
+% conexion(regina, X, Costo)
